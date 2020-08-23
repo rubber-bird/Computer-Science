0371c42a2d94834c958d1cd1a90c808d2329b15e
Marko Turchyn <>
2020-08-22 22:17:31 -0400

    Add bubbleSort function

---
+++ algorithms/sort/bubble_sort.py
@@ -0,0 +1,8 @@
+
+#! /usr/bin/python3
+
+def bubbleSort(arr):
+  for i in range(len(arr) - 1):
+    for j in range(len(arr) - 1 - i):
+      if arr[j] > arr[j+1]:
+        arr[j], arr[j+1] = arr[j+1], arr[j]
