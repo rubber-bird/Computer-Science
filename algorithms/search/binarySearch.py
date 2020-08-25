e8afcd556e998f934a754628cc422df88a70037e
Marko Turchyn <>
2020-08-24 22:36:06 -0400

    Add binary search algorithm function

---
+++ algorithms/search/binarySearch.py
@@ -0,0 +1,16 @@
+#! usr/bin/python3
+
+def binarySearch(arr, l, r, x):
+  if r >= l:
+    mid = l + (r-1)
+    if arr[mid] == x:
+      return mid
+
+    elif arr[mid] > x:
+      return binarySearch(arr, l, mid-1, x)
+
+    else:
+      return binarySearch(arr, mid+1, r, x)
+
+  else:
+    return -1
