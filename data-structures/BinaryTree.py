2900d2f9ecf685c2e370a58e8a9e15923ca5ff42
Marko Turchyn <marko.turchyn@gmail.com>
2020-09-08 21:38:34 -0400

    Binary tree added

---
+++ data-structures/BinaryTree.py
@@ -0,0 +1,24 @@
+#!/bin/env/python3
+
+class Node(object):
+	"""docstring for Node"""
+	def __init__(self, data):
+		self.left = None
+		self.right = None
+		self.data = data
+
+class BinaryTree:
+	def __init__(self):
+		self.root = None
+
+	def getRoot(self):
+		self.root
+
+	def insert(self):
+		pass
+
+	def printTree(self):
+		pass
+
+	def find(self):
+		pass
